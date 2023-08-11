09f8a9b6adea2141c16b5802de4b9ef1a354d3a6
faraguti <faraguti@gmail.com>
2023-08-10 19:15:48 -0600

    Initial commit

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-state-faraguti"
-#     key = "global/s3/terraform.tfstate"
-#     dynamodb_table = "terraform-state"
-#     region = "us-west-2"
-#     encrypt = true
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-faraguti"
+    key = "global/s3/terraform.tfstate"
+    dynamodb_table = "terraform-state"
+    region = "us-west-2"
+    encrypt = true
     
-#     profile = "terraform"
-#   }
-# }
+    profile = "terraform"
+  }
+}
